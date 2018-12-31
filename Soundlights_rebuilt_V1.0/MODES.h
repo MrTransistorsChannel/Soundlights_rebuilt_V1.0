8234315b262c0aad0ac869d94b18db480407e0f0
Mr Transistor <34776474+MrTransistorsChannel@users.noreply.github.com>
2018-12-31 15:41:24 +0300

    Add files via upload

---
+++ Soundlights_rebuilt_V1.0/MODES.h
@@ -0,0 +1,144 @@
+byte MODE, VUcolor, thisHue;
+
+int VU;
+
+uint64_t colorTime, modeTime, hueTime;
+uint16_t timeForColor, timeForMode;
+
+void analyzeSound() {
+  VU = 0;
+  cli();  // UDRE interrupt slows this way down on arduino1.0
+  for (int i = 0 ; i < FHT_N ; i++) { // save 256 samples
+    int k = analogRead(A0);
+    k -= 512; // form into a signed int
+    VU = max(k, VU);
+    k <<= 6; // form into a 16b signed int
+    fht_input[i] = k; // put real data into bins
+  }
+  fht_window(); // window the data for better frequency response
+  fht_reorder(); // reorder the data before doing the fht
+  fht_run(); // process the data in the fht
+  fht_mag_lin8(); // take the output of the fht
+  sei();
+}
+
+void fullSizeSpectrum() {
+  for (byte i = 0 ; i < NUM_LEDS / 2 ; i++) {
+    leds[i] = CHSV(i * (FHT_N / NUM_LEDS) * 2, 255, fht_lin_out8[i * (FHT_N / NUM_LEDS)] * 8); // send out the data
+  }
+  for (byte i = NUM_LEDS / 2; i < NUM_LEDS; i++) {
+    leds[i] = CHSV((NUM_LEDS - (i * (FHT_N / NUM_LEDS))) * 2, 255, fht_lin_out8[NUM_LEDS - (i * (FHT_N / NUM_LEDS))] * 8); // send out the data
+  }
+}
+
+/*void VUMeter(CRGB color) {
+  int val = constrain(map(VU, 0, 500, 0, NUM_LEDS / 2), 0, NUM_LEDS / 2);
+  for (int i = NUM_LEDS / 2; i < (NUM_LEDS / 2) + val; i++) {
+    leds[i] = color;
+  }
+
+  for (int i = (NUM_LEDS / 2) - val; i < NUM_LEDS / 2; i++) {
+    leds[i] = color;
+  }
+
+
+  for (int i = val + (NUM_LEDS / 2); i < NUM_LEDS; i++) {
+    leds[i] = 0;
+  }
+  for (int i = 0; i < (NUM_LEDS / 2) - val; i++) {
+    leds[i] = 0;
+  }
+}*/
+
+void VUMeter(CRGB color) {
+  int val = constrain(map(VU, 0, 500, 0, NUM_LEDS), 0, NUM_LEDS);
+  for (int i = 0; i < val; i++) {
+    leds[i] = color;
+  }
+
+  for (int i = val; i < NUM_LEDS; i++) {
+    leds[i] = 0;
+  }
+}
+
+void peakFlow() {
+  delay(5);
+  for (int i = NUM_LEDS - 2; i >= 0; i--) {
+    leds[i + 1] = leds[i];
+  }
+  if (VU > 500) leds[0] = CRGB::White;
+  else leds[0] = 0;
+}
+
+void threeBarSpectrum() {
+  for (byte i = 0; i < NUM_LEDS / 3; i++) {
+    leds[i] = CHSV(0, 255, fht_lin_out8[0] * 4); // send out the data
+  }
+  for (byte i = NUM_LEDS / 3; i < (NUM_LEDS / 3) * 2; i++) {
+    leds[i] = CHSV(85, 255, fht_lin_out8[FHT_N / 6] * 5); // send out the data
+  }
+  for (byte i = (NUM_LEDS / 3) * 2; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(171, 255, fht_lin_out8[(FHT_N / 6) * 2] * 6); // send out the data
+  }
+
+}
+
+void fiveBarSpectrum() {
+  for (byte i = 0; i < NUM_LEDS / 5; i++) {
+    leds[i] = CHSV(160, 255, fht_lin_out8[(FHT_N / 6) * 2] * 6); // send out the data
+  }
+  for (byte i = NUM_LEDS / 5; i < (NUM_LEDS / 5) * 2; i++) {
+    leds[i] = CHSV(96, 255, fht_lin_out8[FHT_N / 6] * 5); // send out the data
+  }
+  for (byte i = (NUM_LEDS / 5) * 2; i < (NUM_LEDS / 5) * 3; i++) {
+    leds[i] = CHSV(0, 255, fht_lin_out8[0] * 4); // send out the data
+  }
+  for (byte i = (NUM_LEDS / 5) * 3; i < (NUM_LEDS / 5) * 4; i++) {
+    leds[i] = CHSV(96, 255, fht_lin_out8[FHT_N / 6] * 5); // send out the data
+  }
+  for (byte i = (NUM_LEDS / 5) * 4; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(160, 255, fht_lin_out8[(FHT_N / 6) * 2] * 6); // send out the data
+  }
+}
+
+/*void rainbowVU() {
+  int val = constrain(map(VU, 0, 500, 0, NUM_LEDS / 2), 0, NUM_LEDS / 2);
+
+  fill_rainbow(leds, NUM_LEDS, thisHue);
+  if (millis() - hueTime > 5) thisHue++;
+
+  for (int i = val + (NUM_LEDS / 2); i < NUM_LEDS; i++) {
+    leds[i] = 0;
+  }
+  for (int i = 0; i < (NUM_LEDS / 2) - val; i++) {
+    leds[i] = 0;
+  }
+}*/
+
+void rainbowVU() {
+  int val = constrain(map(VU, 0, 500, 0, NUM_LEDS), 0, NUM_LEDS);
+
+  fill_rainbow(leds, NUM_LEDS, thisHue);
+  thisHue++;
+
+  for (int i = val; i < NUM_LEDS; i++) {
+    leds[i] = 0;
+  }
+}
+
+#ifdef WITH_STROBO
+void flowingStrobo() {
+  for (int i = NUM_LEDS / 2; i < NUM_LEDS; i += 2) {
+    leds[i] = CRGB::White;
+    leds[NUM_LEDS - i] = CRGB::White;
+    //delayMicroseconds(300);
+    FastLED.show();
+  }
+  for (int i = NUM_LEDS / 2; i < NUM_LEDS; i += 2) {
+    leds[i] = 0;
+    leds[NUM_LEDS - i] = 0;
+    //delayMicroseconds(300);
+    FastLED.show();
+  }
+}
+#endif
---
+++ Soundlights_rebuilt_V1.0/Soundlights_rebuilt_V1.0.ino
@@ -0,0 +1,114 @@
+#include <FastLED.h>
+
+#define NUM_LEDS 240
+#define DATA_PIN 3
+
+//#define WITH_STROBO
+
+#define LIN_OUT8 1 // use the log output function
+#define SCALE   255
+#define FHT_N   256 // set to 256 point fht
+
+#include <FHT.h> // include the library
+
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+
+CRGB leds[NUM_LEDS];
+
+#include "MODES.h"
+
+void setup() {
+  Serial.begin(9600);
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(255);
+  randomSeed(analogRead(A1));
+  analogReference(EXTERNAL);
+  sbi(ADCSRA, ADPS2);
+  cbi(ADCSRA, ADPS1);
+  sbi(ADCSRA, ADPS0);
+}
+
+void loop() {
+
+  analyzeSound();
+#ifdef WITH_STROBO
+  if (VU < 511 || MODE == 2) {
+
+    switch (MODE) {
+      case 0:
+        generateRandomColor();
+        VUMeter(CHSV(VUcolor, 255, 255));
+        break;
+      case 1:
+        fullSizeSpectrum();
+        break;
+      case 2:
+        peakFlow();
+        break;
+      case 3:
+        threeBarSpectrum();
+        break;
+      case 4:
+        fiveBarSpectrum();
+        break;
+      case 5:
+        rainbowVU();
+        break;
+    }
+
+    modeChangeHandler();
+
+
+
+    FastLED.show();
+  }
+  else flowingStrobo();
+#else
+  switch (MODE) {
+    case 0:
+      generateRandomColor();
+      VUMeter(CHSV(VUcolor, 255, 255));
+      break;
+    case 1:
+      fullSizeSpectrum();
+      break;
+    case 2:
+      peakFlow();
+      break;
+    case 3:
+      threeBarSpectrum();
+      break;
+    case 4:
+      fiveBarSpectrum();
+      break;
+    case 5:
+      rainbowVU();
+      break;
+  }
+
+  modeChangeHandler();
+#endif
+
+  FastLED.show();
+
+
+}
+
+void modeChangeHandler() {
+  if (millis() - modeTime > timeForMode) {
+    byte curMode = MODE;
+    while (MODE == curMode) MODE = random(1000) % 6;
+    Serial.println(MODE);
+    timeForMode = random(4000, 10000);
+    modeTime = millis();
+  }
+}
+
+void generateRandomColor() {
+  if (millis() - colorTime > timeForColor) {
+    VUcolor = random(255);
+    timeForColor = random(2500, 10000);
+    colorTime = millis();
+  }
+}
